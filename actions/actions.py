57e7e233e97440ced19b4db2854b1c8c909b6297
YuriTarded12 <57156001+YuriTarded12@users.noreply.github.com>
2024-04-11 15:14:11 -0400

    First Action

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,56 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+
+class ActionTopicCourse(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # get slot info
+        topic = tracker.get_slot('topic')
+
+        
+        # query
+        sparql = SPARQLWrapper("http://localhost:3030/#/dataset/Data/query", agent='Rasabot agent')
+        sparql.setQuery("""
+            prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+            prefix foaf: <http://xmlns.com/foaf/0.1/>
+            prefix dbo: <http://dbpedia.org/ontology/>
+            prefix uni: <http://uni.com/schema#>
+            prefix owl: <http://www.w3.org/2002/07/owl#> 
+
+
+            SELECT ?cName
+            WHERE{
+            ?course uni:has_topic ?topic.
+
+            ?topic uni:topicName ?tName.
+            ?course uni:subject ?cName.
+            FILTER(REGEX(STR(?tName), '%s', "i"))
+            }
+            LIMIT 100
+
+
+            """ % (topic))
+        sparql.setReturnFormat(JSON)
+        result = sparql.query().convert()
+        for res in result['results']['bindings']:
+            dispatcher.utter_message(text="Course: " + res["cName"]["value"])
+        
+
+
+        
+        return [Restarted()]
